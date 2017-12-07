ffb6b3cb046e465494f865792b1e5d4dac10cbeb
Brandon Campbell <brandoncampbell@Brandons-MacBook-Pro-13.local>
2017-12-06 22:50:35 -0500

    Days 5 and 6

---
+++ 2017/day05/step1.py
@@ -0,0 +1,30 @@
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+f = open("input.txt", "r")
+
+result = 0
+
+array = []
+
+for line in f:
+    print(line.rstrip())
+    if RepresentsInt(line.rstrip()):
+        array.append(int(line.rstrip()))
+
+i = 0
+
+print(len(array))
+while i >= 0 and i < len(array):
+    result = result + 1
+    old = i
+    i = i + array[i]
+    array[old] = array[old] + 1
+
+print(result)
+
+f.close()
---
+++ 2017/day06/step1.py
@@ -0,0 +1,84 @@
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+def canGet(dict, x):
+    try:
+        value = dict[x]
+        return True
+    except:
+        return False
+
+def getStringKey(array):
+    stringKey = ""
+    for value in array:
+        if len(stringKey) > 0:
+            stringKey = stringKey + ","
+        stringKey = stringKey + str(value)
+
+    return stringKey
+
+f = open("input.txt", "r")
+
+result = 0
+
+input = f.read()
+dataArray = []
+
+highIndex = 0
+highValue = 0
+i = 0
+
+for c in input.split("\t"):
+    if RepresentsInt(c):
+        value = int(c)
+        dataArray.append(value)
+        if value > highValue:
+            highIndex = i
+            highValue = value
+        i = i + 1
+
+snapshots = { }
+
+while not canGet(snapshots, getStringKey(dataArray)):
+    print(dataArray)
+    snapshots[getStringKey(dataArray)] = 1
+    print("High value: " + str(highValue) + ", high index: " + str(highIndex))
+    count = dataArray[highIndex]
+    dataArray[highIndex] = 0
+
+
+    index = 0
+    if highIndex + 1 < len(dataArray):
+        index = highIndex + 1
+
+    while count > 0:
+        print("Current index: " + str(index) + ", highValue: " + str(highValue))
+        dataArray[index] = dataArray[index] + 1
+       
+        if index + 1 < len(dataArray):
+            index = index + 1
+        else:
+            index = 0
+
+        count = count - 1
+
+    highValue = 0
+    highIndex = 0
+    i = 0
+
+    for value in dataArray:
+        if dataArray[i] > highValue:
+            highIndex = i
+            highValue = dataArray[i]
+        i = i + 1
+
+    result = result + 1
+
+print(dataArray)
+print(result)
+
+f.close()
