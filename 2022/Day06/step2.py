2ff6f66b181d4bfd3f66a225425bab5dc3a521e1
Brandon Campbell <ruprect@ruprect.com>
2022-12-06 20:26:15 -0500

    Day 06 2022

---
+++ 2022/Day06/step2.py
@@ -0,0 +1,21 @@
+f = open("input.txt", "r")
+input = f.read()
+position = 0
+buffer = ""
+for c in input:
+    buffer += c
+    found = -1
+    if len(buffer) > 13:
+        string = buffer[len(buffer)-14:]
+        for test in string:
+            stringToTest = string.replace(test, '')
+            if len(stringToTest) < 13:
+                found = 0
+        
+        if found == -1:
+            break
+    position += 1
+print(position + 1)
+
+f.close()
+
